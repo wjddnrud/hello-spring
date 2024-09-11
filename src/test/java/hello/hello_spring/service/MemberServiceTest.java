23e3b8bde0b24cf614c0c4de416a9127a64d508a
wookyeong <dnwk203@naver.com>
2024-09-12 02:42:42 +0900

    [스프링 웹 개발] - 회원 관리 예제  1. 회원 서비스 테스트 코드 작성

---
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,83 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepositoy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+    MemberService memberService;
+    MemoryMemberRepositoy memberRepositoy;
+
+    @BeforeEach
+    public void beforeEach() {
+        //이와같이 외부에서 memberService에 파라미터로 memberRepository를 넣어주는것을 Dependency Injection DI라고 한다.
+        memberRepositoy = new MemoryMemberRepositoy();
+        memberService = new MemberService(memberRepositoy);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepositoy.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+        //then
+
+    }
+
+    @Test
+    void 전체회원조회() {
+    }
+
+    @Test
+    void 회원단건조회() {
+    }
+}
