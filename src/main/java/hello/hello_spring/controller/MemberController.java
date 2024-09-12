57b4dd77dcf8c459a0e330935c3192fc8e30ce57
wookyeong <dnwk203@naver.com>
2024-09-12 17:59:13 +0900

    [스프링 웹 개발] - 회원 관리 예제(웹 MVC 개발)  1. 홈 화면 추가  2. 등록  3. 조회

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.dto.MemberDto;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +19,28 @@ public class MemberController {
     @Autowired  // 의존성 주입 방식중 생성자 주입 방식을 권장
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String newMember(Model model, MemberDto memberDto) {
+        Member member = new Member();
+        member.setName(memberDto.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members", memberList);
+
+        return "members/memberList";
     }
 }
